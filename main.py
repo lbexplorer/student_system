3549089dd6b99910cf645342334966c163faba6f
liu'bo <lclw4659@163.com>
2023-03-22 19:46:27 +0800

    第二次修改3.21

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # -*- coding: utf-8 -*-
 student = {'name': '', 'number': ''}
 students = []
+find_student = []
 
 
 # 用students表示有多个学生，每个学生的信息用一个字典表示，student中包含一个学生的姓名和学号（暂时）
@@ -26,36 +27,43 @@ def add_student():
 
 def search_student():
     # 按名字搜索和按学号搜索，先确定选取哪种方法
-    another = input("how to search number/name")
-
-    if another.lower() == 'name':
-        need_to_find = input(
-            "please input need to find {}".format(another))  # another可以是名字或者学号,将another与字典中所有属性相匹配若符合则返回
-        return search(need_to_find)
-
-    elif another.lower() == 'number':
-        need_to_find = int(input(
-            "please input need to find {}".format(another)) ) # another可以是名字或者学号,将another与字典中所有属性相匹配若符合则返回
-        return search(need_to_find)
-
-
-def search(need_to_find):
-    while True:#3.20小bug此处需要修改，实现多次重复查询
+    while True:
         try:
-            for student in students:
-                if student['name'] == need_to_find or student['number'] == need_to_find:
-                    print("name: ", student['name'])
-                    print("number: ", student['number'])
-                    return student
-                    print("\n")
-                else:
-                    print("no find")
+            another = input("how to search number/name")
+            if another.lower() == 'name':
+                need_to_find = input(
+                    "please input need to find {}".format(another))  # another可以是名字或者学号,将another与字典中所有属性相匹配若符合则返回
+            temp=search(need_to_find)
+            if temp!=0:
+                find_student.append(temp.copy())
+
+            elif another.lower() == 'number':
+                need_to_find = int(input("please input need to find {}".format(another)))
+            temp = search(need_to_find)
+            if temp != 0:
+                find_student.append(temp.copy())
+
             another = input("Whether to continue find (y/n)")
             if another.lower() == 'n':
                 break
-        except:
-            print("have error")
-            pass
+        except ValueError:
+            print("输入错误，请重新输入！")
+            continue
+
+
+def search(need_to_find):
+    flag = 1  # flag为1时表示没有找到目标学生
+    for student in students:
+        if student['name'] == need_to_find or student['number'] == need_to_find:
+            print("name: ", student['name'])
+            print("number: ", student['number'])
+            print("\n")
+            flag = 0
+            return student
+
+    if flag:
+        print("no find ")
+        return 0
 
 
 def delete_student():
@@ -64,7 +72,7 @@ def delete_student():
         need_to_find = input("please input need to find {}".format(temp))
         delete(need_to_find)
     elif temp.lower() == 'number':
-        need_to_find = int(input("please input need to find {}".format(temp)))#与查找信息原理相同，先找到再删除
+        need_to_find = int(input("please input need to find {}".format(temp)))  # 与查找信息原理相同，先找到再删除
         delete(need_to_find)
 
 
@@ -77,8 +85,8 @@ def delete(need_to_find):
 
 
 def show_student():
-    aim_student={}
-    aim_student=search_student()
+    aim_student = {}
+    search_student()
     print("name: ", student['name'])
     print("number: ", student['number'])
     pass
